3fac61bbf9992658fea49c55a3130870ab5caafd
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-10-19 18:58:15 +0200

    add migration

--- truffle.js
+++ truffle.js
@@ -15,4 +15,48 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: {
+    // for ganache-gui
+    ganache: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '5777'
+    },
+    kovan: {
+      gas: 4700000,
+      host: 'localhost',
+      network_id: '42',
+      port: 8545
+    },
+    localhost: {
+      host: 'localhost',
+      port: 8546,
+      network_id: '*'
+    },
+    mainnet: {
+      host: 'localhost',
+      port: 8545,
+      gas: 4612388,
+      network_id: '1',
+      from: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f'
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '4',
+      gas: 6700000
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8545,
+      gas: 4612388,
+      network_id: '3'
+    }
+  }
 }
